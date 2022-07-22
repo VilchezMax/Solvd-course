63253d9802a506d2f2f577c79a9ebeccadb5c900
VilchezMax <maximiliano.vilchez@outlook.com>
2022-07-22 12:19:58 -0300

    corrected task3 -selecion sort, optimised loops

--- homework1/src/homework1/task3/insertionsort/InsertionSort.java
+++ homework1/src/homework1/task3/insertionsort/InsertionSort.java
@@ -1,10 +1,10 @@
-package homework1.task3.bubblesort;
+package homework1.task3.insertionsort;
 /*
 3) Task 3
 You have array of numbers [3, 7, 6, 13, 33, 9, -100, 25]
-Sort this array using one of sorting algorithms: BUBBLE SORT
+Sort this array using one of sorting algorithms: INSERTION SORT
  */
-public class BubbleSort {
+public class InsertionSort {
     public static void main(String[] args) {
         int[] array= {3, 7, 6, 13, 33, 9, -100, 25};
 
